74d3e6670ec56b6be5999a1a4abe18529dfd53cd
Shaadi Alfred <shico2000@gmail.com>
2022-02-02 00:09:46 +0200

    Undo fix

--- udagram-frontend/src/app/api/api.service.ts
+++ udagram-frontend/src/app/api/api.service.ts
@@ -55,7 +55,7 @@ export class ApiService {
   }
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
-    const signed_url = (await this.get(`${API_HOST}${endpoint}/signed-url/${file.name}`)).url;
+    const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`)).url;
 
     const headers = new HttpHeaders({'Content-Type': file.type});
     const req = new HttpRequest( 'PUT', signed_url, file,
